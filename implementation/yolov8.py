5de6fb081aa14902df2246266f75ed5187961faa
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2024-06-13 11:53:20 +0800

    Update implementation and requirements; allow video input

--- implementation/yolov8.py
+++ implementation/yolov8.py
@@ -1,7 +1,9 @@
-import pathlib
+import pathlib, mimetypes
 from ultralytics import YOLO
 from .abstraction import FilePath
 from . import abstraction
+from moviepy.editor import VideoFileClip
+
 
 MODULE_DIR = pathlib.Path(__file__).parent
 OUTPUT_DIR = MODULE_DIR / 'output'
@@ -15,23 +17,52 @@ class YOLOv8Model(abstraction.Model):
         self.model: YOLO = model
 
     def infer(self, input_file_paths: list[str]) -> abstraction.InferenceResult:
+
         # the yolov8 model expects a single input file
         input_file_path = input_file_paths[0]
+
+        mimeType, encoding = mimetypes.guess_type(input_file_path)
         
-        # START: model inference logic
-        results = self.model.predict(source=str(input_file_path), save=True, project=str(OUTPUT_DIR))
+        mediaType = mimeType.split('/')[0]
 
-        save_dir = pathlib.Path(results[0].save_dir)
-        prediction_files = [item for item in save_dir.iterdir() if item.is_file]
-        prediction_output_file = prediction_files[0]
+        if mediaType == 'image':
+            # START: model inference logic
+            results = self.model.predict(source=str(input_file_path), save=True, project=str(OUTPUT_DIR))
 
-        # END: model inference logic
+            save_dir = pathlib.Path(results[0].save_dir)
+            prediction_files = [item for item in save_dir.iterdir() if item.is_file]
+            prediction_output_file = prediction_files[0]
 
-        return {
-            'data': prediction_output_file, # the file path of the output
-            'info': {'any': 'info'}, # any information related to the inference result
-            'type': "image/png" # MIME type of the output
-        }
+            # END: model inference logic
+
+            return {
+                'data': prediction_output_file, # the file path of the output
+                'info': {'any': 'info'}, # any information related to the inference result
+                'type': "image/png" # MIME type of the output
+            }
+        
+        elif mediaType == 'video':
+            # START: model inference logic
+            results = self.model.predict(source=str(input_file_path), save=True, project=str(OUTPUT_DIR))
+
+            save_dir = pathlib.Path(results[0].save_dir)
+            prediction_files = [item for item in save_dir.iterdir() if item.is_file]
+            prediction_output_file = prediction_files[0]
+
+            prediction_output_file_mp4_path = prediction_output_file.with_suffix('.mp4')
+            # Load the video file
+            clip = VideoFileClip(str(prediction_output_file))
+            
+            # Write the video file to MP4 format
+            clip.write_videofile(str(prediction_output_file_mp4_path), codec="libx264")
+
+            # END: model inference logic
+
+            return {
+                'data': prediction_output_file_mp4_path, # the file path of the output
+                'info': {'any': 'info'}, # any information related to the inference result
+                'type': "video/mp4" # MIME type of the output
+            }
 
 class YOLOv8ModelBuilder(abstraction.ModelBuilder):
     """
@@ -51,6 +82,6 @@ class YOLOv8ModelBuilder(abstraction.ModelBuilder):
 builder_class = YOLOv8ModelBuilder
 
 inference_metadata = {
-    'input_files': [ ['image'] ], # requires a single input file with an image type
+    'input_files': [ ['image', 'video'] ], # requires a single input file with an image type
     'model_artifacts': [ ['.pt'] ] # requires one model artifact with a .pt file extension
 }
